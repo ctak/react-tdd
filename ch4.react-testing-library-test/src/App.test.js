462610d434e8ca5b6b32be127e55d9894b2ca43b
Changbeam Tak <tak.changbeom@gmail.com>
2022-06-03 17:22:40 +0900

    first commit

---
+++ ch4.react-testing-library-test/src/App.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('<App />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+    expect(container.getElementsByClassName('App-logo'))
+      .toHaveLength(1);
+
+    expect(container.getElementsByClassName('App-logo')[0])
+      .toHaveAttribute(
+        'src',
+        'logo.svg'
+      );
+
+    expect(container.getElementsByTagName('p')).toHaveLength(1);
+    expect(container.getElementsByTagName('p')[0])
+      .toHaveTextContent(
+        'Edit src/App.js and save to reload.'
+      );
+
+    // 스냅샷 테스트
+    expect(container).toMatchSnapshot();
+  });
+});
