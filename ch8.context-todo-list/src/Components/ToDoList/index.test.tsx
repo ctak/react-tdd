bf37db1d7b02a55dadfb65eb34b51af1bb55e44e
ctak <tak.changbeom@gmail.com>
2022-07-20 09:27:04 +0900

    [context-todo-list] 완성

--- ch8.context-todo-list/src/Components/ToDoList/index.test.tsx
+++ ch8.context-todo-list/src/Components/ToDoList/index.test.tsx
@@ -33,5 +33,21 @@ describe('<ToDoList />', () => {
         expect(screen.getByText('ToDo 2')).toBeInTheDocument();
         expect(screen.getByText('ToDo 3')).toBeInTheDocument();
         expect(screen.getAllByText('삭제').length).toBe(3);
+    });
+
+    it('deletes toDo item', () => {
+        localStorage.setItem('ToDoList', '["ToDo 1", "ToDo 2", "ToDo 3"]');
+
+        render(
+            <ToDoListProvider>
+                <ToDoList />
+            </ToDoListProvider>
+        );
+
+        const toDoItem = screen.getByText('ToDo 2');
+        expect(toDoItem).toBeInTheDocument();
+        fireEvent.click(toDoItem.nextElementSibling as HTMLElement);
+        expect(toDoItem).not.toBeInTheDocument();
+        expect(JSON.parse(localStorage.getItem('ToDoList') as string)).not.toContain('ToDo 2');
     })
 })
