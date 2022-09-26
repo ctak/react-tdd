1c19b5621c272151cbae9e06e6fb7070d6b4f4ff
Changbeam Tak <tak.changbeom@gmail.com>
2022-09-26 13:41:43 +0900

    [ch321.blog/blog-frontend] 25.3.3 글쓰기 API 연동하기

--- ch321.blog/blog-frontend/src/modules/write.js
+++ ch321.blog/blog-frontend/src/modules/write.js
@@ -1,7 +1,15 @@
 import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, { createRequestActionTypes } from '../lib/asyncUtils';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
 
 const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; // 특정 key 값 바꾸기
+const [
+  WRITE_POST,
+  WRITE_POST_SUCCESS,
+  WRITE_POST_FAILURE,
+] = createRequestActionTypes('write/WRITE_POST');
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -9,10 +17,25 @@ export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
   value,
 }));
 
+// 액션은 순수 함수. dispatch 된다는 것이지. ({ title, body, tags } = action.payload);
+export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
+  title,
+  body,
+  tags,
+}));
+
+// 사가 생성
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+export function* writeSaga() {
+  yield takeLatest(WRITE_POST, writePostSaga);
+}
+
 const initialState = {
   title: '',
   body: '',
   tags: [],
+  post: null,
+  postError: null,
 };
 
 const write = handleActions(
@@ -21,6 +44,20 @@ const write = handleActions(
     [CHANGE_FIELD]: (state, { payload: {key, value} }) => ({
       ...state,
       [key]: value, // 특정 key 값을 업데이트
+    }),
+    [WRITE_POST]: state => ({
+      ...state,
+      // post 와 postError 를 초기화
+      post: null,
+      postError: null,
+    }),
+    [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post,
+    }),
+    [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
+      ...state,
+      postError,
     })
   },
   initialState,
