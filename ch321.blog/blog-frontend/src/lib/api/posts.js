eee1f29211f222f452807652e150d8e5e303d186
Changbeam Tak <tak.changbeom@gmail.com>
2022-09-26 17:21:35 +0900

    [ch321.blog/blog-frontend] 26. 포스트 읽기 API 연동하기

--- ch321.blog/blog-frontend/src/lib/api/posts.js
+++ ch321.blog/blog-frontend/src/lib/api/posts.js
@@ -3,3 +3,6 @@ import client from './client';
 // 글쓰기
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+// 포스트 읽기
+export const readPost = id => client.get(`/api/posts/${id}`);
