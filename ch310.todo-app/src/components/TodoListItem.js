f0ab617c4635f5e946df8f2c334ebc0ffb4cd79f
Changbeam Tak <tak.changbeom@gmail.com>
2022-10-26 22:21:31 +0900

    [ch310.todo-app] react-virtualized 적용

--- ch310.todo-app/src/components/TodoListItem.js
+++ ch310.todo-app/src/components/TodoListItem.js
@@ -7,19 +7,21 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss'
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
-        { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank /> }
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove">
-        <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
+          { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank /> }
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove">
+          <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 // 이제 TodoListItem 컴포넌트는 todo, onRemove, onToggle 이 바뀌지 않으면 리렌더링을 하지 않습니다.
