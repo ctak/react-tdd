7395038a0172fa170e4fa764074023431460addb
ctak <tak.changbeom@gmail.com>
2022-11-22 15:24:41 +0900

    [ch319.splitting-sample] @loadable/component 로 서버 사이드 렌더링 도 하면서 코드 스플리팅 하자

--- ch319.splitting-sample/src/App.js
+++ ch319.splitting-sample/src/App.js
@@ -1,25 +1,101 @@
 import logo from './logo.svg';
 import './App.css';
 
+/* // 함수를 spliting 하기
+// import notify from './notify';
 function App() {
+  const onClick = () => {
+    // notify();
+    import('./notify').then(result => result.default());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
 }
+
+*/
+
+/* state 를 사용한 코드 스플리팅 */
+/*
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    SplitMe: null
+  };
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
+}
+*/
+/* React.lazy 와 Suspense 사용하기 */
+/*
+import React, { useState, Suspense } from 'react';
+const SplitMe = React.lazy(() => import('./SplitMe'));
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+};
+*/
+/* Loadable Components 를 통한 코드 스플리팅 */
+import { useState } from 'react';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
+        {visible && <SplitMe />}
+      </header>
+    </div>
+  );  
+};
 
 export default App;
